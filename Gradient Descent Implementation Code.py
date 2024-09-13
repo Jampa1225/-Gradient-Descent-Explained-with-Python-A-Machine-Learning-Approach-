b078341d8da525f55cf7fcd1e606caab2befb1a6
Jampa Rakesh <158861043+Jampa1225@users.noreply.github.com>
2024-09-13 15:28:06 -0700

    Gradient Descent Implementation Code.py

--- Gradient Descent Implementation Code.py
+++ Gradient Descent Implementation Code.py
@@ -11,3 +11,30 @@ Y = data.iloc[:, 1]
 plt.scatter(X, Y)
 plt.show()
 
+# Building the model
+m = 0
+c = 0
+
+L = 0.0001  # The learning Rate
+epochs = 1000  # The number of iterations to perform gradient descent
+
+n = float(len(X)) # Number of elements in X
+
+# Performing Gradient Descent 
+for i in range(epochs): 
+    Y_pred = m*X + c  # The current predicted value of Y
+    D_m = (-2/n) * sum(X * (Y - Y_pred))  # Derivative wrt m
+    D_c = (-2/n) * sum(Y - Y_pred)  # Derivative wrt c
+    m = m - L * D_m  # Update m
+    c = c - L * D_c  # Update c
+    
+print (m, c)
+
+# 1.4796491688889395 0.10148121494753726 # Output
+
+# Making predictions
+Y_pred = m*X + c
+
+plt.scatter(X, Y) 
+plt.plot([min(X), max(X)], [min(Y_pred), max(Y_pred)], color='red')  # regression line
+plt.show()
